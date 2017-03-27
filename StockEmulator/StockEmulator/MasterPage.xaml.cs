abe5402e81ca9f6116e83339f9f5bad788ad73c2
Quách Minh Trí <qmtri@apcs.vn>
2017-03-27 17:18:02 +0700

    Logout function

--- StockEmulator/StockEmulator/MasterPage.xaml.cs
+++ StockEmulator/StockEmulator/MasterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockEmulator.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,32 @@ namespace StockEmulator
                 "Privacy Policy and Discaimer",
                 "About us"
             };
+            logOut.GestureRecognizers.Add(new TapGestureRecognizer((view) => OnLabelClicked()));
+
+
+        }
+
+        private async void OnLabelClicked()
+        {
+            var accepted = await DisplayAlert("Warning!", "Do you want to Logout?", "Yes", "No");
+            if (accepted)
+            {
+
+                // await Navigation.PopAsync();
+                await Navigation.PushModalAsync(new LoginPage());
+                
+
+                //  await Navigation.PopAsync();
+                //var existingPages = Navigation.NavigationStack.ToList();
+                //foreach (var page in existingPages)
+                //{
+                //    Navigation.RemovePage(page);
+                //}
+                //await Navigation.PushModalAsync(new LoginPage());
+                // Navigation.NavigationStack[0];
+
+
+            }
         }
     }
 }
