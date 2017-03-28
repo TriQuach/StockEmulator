45ba5086949f14ad220e605c63246d76d95b2418
Minh-Tri Le <lmtri1995@gmail.com>
2017-03-28 12:25:38 +0700

    Edit X-Axis of 1 Day History Price Chart in StockInfoPage
    Change Time of Day type on X-Axis from discrete to continuous

--- StockEmulator/StockEmulator/Pages/StockInfoPage.xaml.cs
+++ StockEmulator/StockEmulator/Pages/StockInfoPage.xaml.cs
@@ -37,42 +37,48 @@ namespace StockEmulator.Pages
 
             var htmlSource = new HtmlWebViewSource();
 
-            List<ChartDataModel> chartData = new List<ChartDataModel>();
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 11, 3, 17), 463.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 12, 3, 17), 353.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 13, 3, 17), 573.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 14, 3, 17), 393.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 15, 3, 17), 503.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 16, 3, 17), 643.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 17, 3, 17), 613.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 18, 3, 17), 663.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 19, 3, 17), 763.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 20, 3, 17), 633.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 21, 3, 17), 763.37, 757.8));
-            chartData.Add(new ChartDataModel(new DateTime(2017, 3, 11, 22, 3, 17), 863.37, 757.8));
+            List<History1DayModel> chartData = new List<History1DayModel>();
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 11, 6, 20), 463.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 12, 15, 46), 353.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 13, 20, 14), 573.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 13, 49, 33), 393.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 14, 10, 18), 503.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 15, 17, 59), 643.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 16, 33, 28), 613.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 16, 44, 47), 663.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 17, 17, 38), 763.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 18, 28, 33), 633.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 19, 38, 29), 763.37m, 757.8m));
+            chartData.Add(new History1DayModel(new DateTime(2017, 3, 11, 20, 26, 40), 863.37m, 757.8m));
 
             string html = HTMLChartBuilder.BuildHTMLLineChartFor1DayHistory("GOOGL (Alphabet Inc.)", chartData);
             htmlSource.Html = html;
 
 //            htmlSource.Html = @"<html>
 //  <head>
-//    <script type=""text/javascript"" src=""jsapi.js""></script>
-//    <script type=""text/javascript"">
-//       google.load(""visualization"", ""1"", { packages:[""corechart""]});
+//    <script type='text/javascript' src='jsapi.js'></script>
+//    <script type='text/javascript'>
+//       google.load('visualization', '1', { packages:['corechart']});
 //       google.setOnLoadCallback(drawChart);
 //            function drawChart() {
-//                var data = google.visualization.arrayToDataTable([
-        
-//                  ['Year', 'Price', 'Previous Close'],
-        
-//                  ['2010', 1000, 600],
-        
-//                  ['2011', 1170, 600],
-        
-//                  ['2012', 660, 600],
-        
-//                  ['2013', 1030, 600]
-//        ]);
+//                var data = new google.visualization.DataTable();
+//                    data.addColumn('timeofday', 'Time of Day');
+//                    data.addColumn('number', 'Price');
+//                    data.addColumn('number', 'Previous Close Price');
+
+//                data.addRows([
+//                [[8, 30, 45], 5, 6],
+//        [[9, 0, 0], 10, 6],
+//        [[10, 0, 0, 0], 12, 6],
+//        [[10, 45, 0, 0], 13, 6],
+//        [[11, 0, 0, 0], 25, 6],
+//        [[12, 15, 45, 0], 20, 6],
+//        [[13, 0, 0, 0], 22, 6],
+//        [[14, 30, 0, 0], 16, 6],
+//        [[15, 12, 0, 0], 30, 6],
+//        [[16, 45, 0], 32, 6],
+//        [[16, 59, 0], 10, 6]
+//            ]);
 
 //                var options = {
 //          title: 'GOOGL (Alphabet Inc.)',
@@ -94,9 +100,52 @@ namespace StockEmulator.Pages
 //    </script>
 //  </head>
 //  <body>
-//    <div id=""chart_div""></div>
+//    <div id='chart_div'></div>
 //  </body>
 //</html>";
+            //            htmlSource.Html = @"<html>
+            //  <head>
+            //    <script type=""text/javascript"" src=""jsapi.js""></script>
+            //    <script type=""text/javascript"">
+            //       google.load(""visualization"", ""1"", { packages:[""corechart""]});
+            //       google.setOnLoadCallback(drawChart);
+            //            function drawChart() {
+            //                var data = google.visualization.arrayToDataTable([
+
+            //                  ['Year', 'Price', 'Previous Close'],
+
+            //                  ['2010', 1000, 600],
+
+            //                  ['2011', 1170, 600],
+
+            //                  ['2012', 660, 600],
+
+            //                  ['2013', 1030, 600]
+            //        ]);
+
+            //                var options = {
+            //          title: 'GOOGL (Alphabet Inc.)',
+            //          legend: { position: 'bottom' },
+            //            animation: { duration: 1000,
+            //                        easing: 'out'},
+            //            chartArea: { width: '85%', 
+            //                        height: '85%',
+            //                        left: '10%',
+            //                        top: '10%',
+            //                        right: 0,
+            //                        bottom: '15%'},
+            //            series: { 1: { lineDashStyle: [2, 4], lineWidth: 1 } }
+            //            };
+
+            //            var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+            //            chart.draw(data, options);
+            //        }
+            //    </script>
+            //  </head>
+            //  <body>
+            //    <div id=""chart_div""></div>
+            //  </body>
+            //</html>";
             htmlSource.BaseUrl = DependencyService.Get<IBaseUrl>().Get();
 
             ChartView.Source = htmlSource;
