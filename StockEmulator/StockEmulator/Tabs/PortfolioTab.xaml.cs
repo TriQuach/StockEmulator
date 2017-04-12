1ca9f18b12ed3d90a9da5d19554c5314cc79a389
Minh-Tri Le <lmtri1995@gmail.com>
2017-04-12 20:09:14 +0700

    Add Text & Color Binding Converter.
    Add Text Binding & Color Binding Converter to Portfolio & Transactions Tab.

--- StockEmulator/StockEmulator/Tabs/PortfolioTab.xaml.cs
+++ StockEmulator/StockEmulator/Tabs/PortfolioTab.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Globalization;
 
 namespace StockEmulator.Tabs
 {
@@ -31,8 +32,7 @@ namespace StockEmulator.Tabs
                 page.BindingContext = item;
                 await Navigation.PushAsync(page);
             };
-            //loadingPortfolio.IsVisible = true;
-            //loadingPortfolio.IsRunning = true;
+
             listPortfolios.ItemTemplate = new DataTemplate(typeof(CustomPortfolioCell));
 
             StackLayout1.WidthRequest = App.ScreenWidth / 5;
@@ -44,7 +44,7 @@ namespace StockEmulator.Tabs
             GetNewData();
         }
 
-        public class CustomPortfolioCell : ViewCell
+        class CustomPortfolioCell : ViewCell
         {
             public CustomPortfolioCell()
             {
@@ -53,7 +53,7 @@ namespace StockEmulator.Tabs
                     HorizontalOptions = LayoutOptions.FillAndExpand,
                     Orientation = StackOrientation.Vertical,
                     Spacing = 0,
-                    Margin = new Thickness(5,0,0,0),
+                    Margin = new Thickness(5, 0, 0, 0),
                     WidthRequest = App.ScreenWidth / 5
                 };
                 var verticaLayout2 = new StackLayout()
@@ -62,7 +62,6 @@ namespace StockEmulator.Tabs
                     Orientation = StackOrientation.Vertical,
                     Spacing = 0,
                     WidthRequest = App.ScreenWidth / 5
-
                 };
                 var verticaLayout3 = new StackLayout()
                 {
@@ -70,7 +69,6 @@ namespace StockEmulator.Tabs
                     Orientation = StackOrientation.Vertical,
                     Spacing = 0,
                     WidthRequest = App.ScreenWidth / 5
-
                 };
                 var verticaLayout4 = new StackLayout()
                 {
@@ -78,7 +76,6 @@ namespace StockEmulator.Tabs
                     Orientation = StackOrientation.Vertical,
                     Spacing = 0,
                     WidthRequest = App.ScreenWidth / 5
-
                 };
                 var verticaLayout5 = new StackLayout()
                 {
@@ -87,80 +84,89 @@ namespace StockEmulator.Tabs
                     Spacing = 0,
                     Margin = new Thickness(0, 0, 5, 0),
                     WidthRequest = App.ScreenWidth / 5
-
                 };
                 
                 var TickerLabel = new Label()
                 {
-                    FontSize = 11,
+                    FontSize = 13,
                     VerticalOptions = LayoutOptions.Center,
                     TextColor = Color.FromHex("007FFF")
                 };
-                var EquityLabel = new Label()
+                var EquityNameLabel = new Label()
                 {
+                    FontSize = 12,
                     VerticalOptions = LayoutOptions.Center,
                     LineBreakMode = LineBreakMode.TailTruncation,
-                    FontSize = 11,
                     TextColor = Color.FromHex("666666")
                 };
                 var PriceLabel = new Label()
                 {
+                    FontSize = 12,
+                    FontAttributes = FontAttributes.Bold,
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center,
-                    FontSize = 11,
-                    FontAttributes = FontAttributes.Bold
+                    TextColor = Color.FromHex("000000")
                 };
                 var CostLabel = new Label()
                 {
+                    FontSize = 12,
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center,
-                    FontSize = 11
+                    TextColor = Color.FromHex("000000")
                 };
                 var GainLossMoneyLabel = new Label()
                 {
+                    FontSize = 12,
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center,
-                    FontSize = 11,
+                    TextColor = Color.FromHex("000000")
                 };
                 var NumStocksLabel = new Label()
                 {
+                    FontSize = 12,
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center,
-                    FontSize = 11,
+                    TextColor = Color.FromHex("000000")
                 };
                 var ChangeMoneyLabel = new Label()
                 {
+                    FontSize = 12,
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center,
-                    FontSize = 11,
+                    TextColor = Color.FromHex("000000")
                 };
                 var ValueLabel = new Label()
                 {
+                    FontSize = 12,
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.End,
-                    FontSize = 11,
+                    TextColor = Color.FromHex("000000")
                 };
                 var ChangePercentLabel = new Label()
                 {
+                    FontSize = 12,
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.End,
-                    FontSize = 11,
-                    LineBreakMode = LineBreakMode.TailTruncation
+                    TextColor = Color.FromHex("000000")
                 };
                 
                 TickerLabel.SetBinding(Label.TextProperty, new Binding("Ticker"));
-                EquityLabel.SetBinding(Label.TextProperty, new Binding("EquityName"));
-                PriceLabel.SetBinding(Label.TextProperty, new Binding("Price"));
+                EquityNameLabel.SetBinding(Label.TextProperty, new Binding("EquityName"));
+                PriceLabel.SetBinding(Label.TextProperty, new Binding("Price", BindingMode.Default, new PriceTextConverter()));
                 CostLabel.SetBinding(Label.TextProperty, new Binding("Cost"));
-                GainLossMoneyLabel.SetBinding(Label.TextProperty, new Binding("GainLossMoney"));
+                GainLossMoneyLabel.SetBinding(Label.TextProperty, new Binding("GainLossMoney", BindingMode.Default, new GainLossMoneyTextConverter()));
+                GainLossMoneyLabel.SetBinding(Label.TextColorProperty, new Binding("GainLossMoney", BindingMode.Default, new GainLossMoneyColorConverter()));
                 NumStocksLabel.SetBinding(Label.TextProperty, new Binding("NumStocks"));
-                ChangeMoneyLabel.SetBinding(Label.TextProperty, new Binding("ChangeMoney"));
-                ValueLabel.SetBinding(Label.TextProperty, new Binding("Value"));
-                ChangePercentLabel.SetBinding(Label.TextProperty, new Binding("ChangePercent"));
+                ChangeMoneyLabel.SetBinding(Label.TextProperty, new Binding("ChangeMoney", BindingMode.Default, new ChangeMoneyTextConverter()));
+                ChangeMoneyLabel.SetBinding(Label.TextColorProperty, new Binding("ChangeMoney", BindingMode.Default, new ChangeMoneyColorConverter()));
+                ValueLabel.SetBinding(Label.TextProperty, new Binding("Value", BindingMode.Default, new ValueTextConverter()));
+                ValueLabel.SetBinding(Label.TextColorProperty, new Binding("ChangeMoney", BindingMode.Default, new ValueColorConverter()));
+                ChangePercentLabel.SetBinding(Label.TextProperty, new Binding("ChangePercent", BindingMode.Default, new ChangePercentTextConverter()));
+                ChangePercentLabel.SetBinding(Label.TextColorProperty, new Binding("ChangePercent", BindingMode.Default, new ChangePercentColorConverter()));
 
 
                 verticaLayout1.Children.Add(TickerLabel);
-                verticaLayout1.Children.Add(EquityLabel);
+                verticaLayout1.Children.Add(EquityNameLabel);
 
                 verticaLayout2.Children.Add(PriceLabel);
 
@@ -185,6 +191,203 @@ namespace StockEmulator.Tabs
                 horizontalLayout.Children.Add(verticaLayout5);
 
                 View = horizontalLayout;
+            }
+        }
+
+        class PriceTextConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    return string.Format("${0:0.00}", Math.Abs((decimal)value));
+                }
+                return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class GainLossMoneyTextConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    return string.Format("${0:0.000}", Math.Abs((decimal)value));
+                }
+                return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class GainLossMoneyColorConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    if ((decimal)value < 0)
+                    {
+                        return Color.FromRgb(139, 0, 0);
+                    }
+                    if ((decimal)value > 0)
+                    {
+                        return Color.FromRgb(0, 100, 0);
+                    }
+                    if ((decimal)value == 0)
+                    {
+                        return Color.FromRgb(0, 0, 0);
+                    }
+                }
+                return Color.Default;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class ChangeMoneyTextConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    return string.Format("${0:0.000}", Math.Abs((decimal)value));
+                }
+                return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class ChangeMoneyColorConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    if ((decimal)value < 0)
+                    {
+                        return Color.FromRgb(139, 0, 0);
+                    }
+                    if ((decimal)value > 0)
+                    {
+                        return Color.FromRgb(0, 100, 0);
+                    }
+                    if ((decimal)value == 0)
+                    {
+                        return Color.FromRgb(0, 0, 0);
+                    }
+                }
+                return Color.Default;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class ValueTextConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    return string.Format("${0:0.000}", Math.Abs((decimal)value));
+                }
+                return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class ValueColorConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    if ((decimal)value < 0)
+                    {
+                        return Color.FromRgb(139, 0, 0);
+                    }
+                    if ((decimal)value > 0)
+                    {
+                        return Color.FromRgb(0, 100, 0);
+                    }
+                    if ((decimal)value == 0)
+                    {
+                        return Color.FromRgb(0, 0, 0);
+                    }
+                }
+                return Color.Default;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class ChangePercentTextConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    return string.Format("{0:0.000}%", Math.Abs((decimal)value));
+                }
+                return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class ChangePercentColorConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value != null)
+                {
+                    if ((decimal)value < 0)
+                    {
+                        return Color.FromRgb(139, 0, 0);
+                    }
+                    if ((decimal)value > 0)
+                    {
+                        return Color.FromRgb(0, 100, 0);
+                    }
+                    if ((decimal)value == 0)
+                    {
+                        return Color.FromRgb(0, 0, 0);
+                    }
+                }
+                return Color.Default;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
             }
         }
 
